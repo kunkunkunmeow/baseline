c0e27db3fea544fddc1d2afc9134298fc5a5fb1d
simanli12 <56723489+simanli12@users.noreply.github.com>
2019-11-26 18:08:16 +0000

    update to split by section and segment

--- cannibalisation.py
+++ cannibalisation.py
@@ -15,6 +15,12 @@ cb_l = 'segment'
 # flag whether to use negative cannibalisation value 
 cb_np_flag = "all" # "all" or "positive"
 
+# Append or replace destination table (either 'append' or 'replace')
+bl_table_config = 'replace'
+
+# Set batch size
+batchsize = 50
+
 # Set logger properties
 logger = logging.getLogger('cannibalisation_calculation')
 logger.setLevel(logging.DEBUG)
@@ -36,15 +42,33 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
-def load_bl_from_bq(project_id, level):
+def load_section_from_bq(project_id):
     start_time = time.time()
 
     summary_sql = """
-    SELECT cast(DATE(date) AS DATE) AS date, sku_root_id, sku.segment, promo_flag_binary, incremental_qty, cb_flag, cb_sale_amt, cb_sale_qty, cb_margin_amt
+    SELECT distinct section
     FROM `baseline_performance.baseline` 
-    LEFT JOIN (SELECT sku_root_id, %s FROM `ETL.root_sku`) sku
+    LEFT JOIN (SELECT sku_root_id, section FROM `ETL.root_sku`) 
+    USING(sku_root_id) """
+    start = time.time()
+
+    for i in tqdm(range(1), desc='Loading table...'):
+        section_table = pandas_gbq.read_gbq(summary_sql, project_id=project_id)
+
+    total_time = round((time.time() - start_time) / 60, 1)
+    logger.info("Completed loading of distinct sections table from Bigquery {a} mins...".format(a=total_time))
+
+    return section_table
+
+def load_bl_from_bq(project_id, section, level):
+    start_time = time.time()
+
+    summary_sql = """
+    SELECT date, sku_root_id, section, segment, promo_flag_binary, incremental_qty, cb_flag, cb_sale_amt, cb_sale_qty, cb_margin_amt
+    FROM `baseline_performance.baseline`
+    LEFT JOIN (SELECT sku_root_id, section, segment FROM `ETL.root_sku`) 
     USING(sku_root_id)
-    """%(level)
+    WHERE section = "%s"   """ %(section)
 
     for i in tqdm(range(1), desc='Loading table...'):
         baseline_table = pandas_gbq.read_gbq(summary_sql, project_id=project_id)
@@ -55,10 +79,12 @@ def load_bl_from_bq(project_id, level):
     return baseline_table
 
 # define the calculation of cannibalisation for certain date
-def cannibalisation(frame, cb_table, date, level):
-    table = cb_table[['date','sku_root_id', 'promo_flag_binary','incremental_qty', level]][cb_table['date'] == date]
-    
-    df = pd.merge(table, agg_np, on=['date',level])
+def cannibalisation(frame, agg_np, cb_table, cb_l, cb_level):
+    table = cb_table[['date','sku_root_id', 'promo_flag_binary','incremental_qty', cb_l]][cb_table[cb_l] == cb_level]
+
+    agg_np_cb = agg_np[agg_np[cb_l] == 'DESAYUNO TIPO DIGESTIVE']
+
+    df = pd.merge(table, agg_np_cb, on=['date',cb_l])
 
     df['cb_pct'] = df['incremental_qty'] / df['ttl_inc_sale_qty']
     
@@ -75,56 +101,94 @@ def cannibalisation(frame, cb_table, date, level):
 if __name__ == "__main__":
     start_time = time.time()
     logger.info("Loading baseline tables from Bigquery....")
-    baseline_table = load_bl_from_bq(project_id, cb_l)
-    
-    logger.info("Clean up baseline table ...")
-    baseline_table['cb_sale_amt'] = pd.to_numeric(baseline_table['cb_sale_amt'])
-    baseline_table['cb_sale_qty'] = pd.to_numeric(baseline_table['cb_sale_qty'])
-    baseline_table['cb_margin_amt'] = pd.to_numeric(baseline_table['cb_margin_amt'])
-    baseline_table['incremental_qty'] = pd.to_numeric(baseline_table['incremental_qty'])
-    
-    # options to ignore the negative values in the cannibalisation amount
-    cb_table = baseline_table.copy()
-    if cb_np_flag == "positive":
-        num = cb_table._get_numeric_data()
-        num[num < 0] = 0
-    
-    logger.info("aggreate the cannibalisation amount into the defined level")
-    agg_np = cb_table.groupby(["date",cb_l], as_index=False)['incremental_qty','cb_sale_amt', 'cb_sale_qty', 'cb_margin_amt'].sum()
-    agg_np.columns = ['date', cb_l, 'ttl_inc_sale_qty','ttl_cb_sale_amt', 'ttl_cb_sale_qty', 'ttl_cb_margin_amt']
-    
-    #get unique dates 
-    dates = list(baseline_table.date.unique())
-    
-    # Store the baseline results
-    results_df = pd.DataFrame()
+    section_table = load_t0_from_bq(bl_s, project_id)
 
-    with Manager() as manager:
-        frame = manager.list()  # <-- can be shared between processes.
-        processes = []
+    # Unique sections in category include
+    unique_sections = list(section_table["section"].unique())
+    logger.info("Unique sections include:")
+    for section in unique_sections: logger.info("{a}".format(a=section))
 
-        start_time = time.time()
+    # Loop through sections
+    for i_sec in range(0, len(unique_sections)):
+        section_start_time = time.time()
+        section = unique_sections[i_sec]
 
-        for date in dates:
-            p = Process(target=cannibalisation, args=(frame, cb_table, date, cb_l))  # Passing the list
-            p.start()
-            processes.append(p)
-        for p in processes:
-            p.join()
+        logger.info("Processing section {a}...".format(a=section))
 
-        output = pd.concat(frame)
-        results_df = pd.concat([results_df, output], ignore_index=True, sort =False)
-        results_df.reset_index(drop=True, inplace=True)
+        # load baseline table for each section     
+        logger.info("Loading summary transaction table from Bigquery....")
+        baseline_table = load_bl_from_bq(project_id, section, cb_l)
+    
+        logger.info("Clean up baseline table ...")
+        baseline_table['cb_sale_amt'] = pd.to_numeric(baseline_table['cb_sale_amt'])
+        baseline_table['cb_sale_qty'] = pd.to_numeric(baseline_table['cb_sale_qty'])
+        baseline_table['cb_margin_amt'] = pd.to_numeric(baseline_table['cb_margin_amt'])
+        baseline_table['incremental_qty'] = pd.to_numeric(baseline_table['incremental_qty'])
+    
+        # options to ignore the negative values in the cannibalisation amount
+        cb_table = baseline_table.copy()
+        if cb_np_flag == "positive":
+            num = cb_table._get_numeric_data()
+            num[num < 0] = 0
+    
+        logger.info("aggreate the cannibalisation amount into the defined level")
+        agg_np = cb_table.groupby(["date",cb_l], as_index=False)['incremental_qty','cb_sale_amt', 'cb_sale_qty', 'cb_margin_amt'].sum()
+        agg_np.columns = ['date', cb_l, 'ttl_inc_sale_qty','ttl_cb_sale_amt', 'ttl_cb_sale_qty', 'ttl_cb_margin_amt']
+    
+        #get unique dates 
+        logger.info("Computing no. of unique in-scope dates")
+        dates = list(baseline_table.date.unique())
 
-        total_time = round((time.time() - start_time), 2)
-        logger.debug('Processing took {b} secs...'.format(b=total_time))
-        logger.info('Results dataframe has {a} rows and {b} cols...'.format(a=results_df.shape[0], b=results_df.shape[1]))
-      
-    # Convert all nulls to None
-    results_df = results_df.where((pd.notnull(results_df)), None)
+        logger.info("Computing no. of unique in-scope cb_l")
+        unique_cb_l = list(baseline_table[cb_l].unique())
+    
+        # Store the baseline results
+        results_df = pd.DataFrame()
+
+        with Manager() as manager:
+            frame = manager.list()  # <-- can be shared between processes.
+            processes = []
+
+            for i in range(0,len(unique_cb_l), batchsize):
+                 # Clear the processes list
+                processes[:] = []
+
+                start_time_batch = time.time()
+                batch = uniq_cb_l[i:i+batchsize] # the result might be shorter than batchsize at the end
+
+                for cb_level in batch:
+                    p = Process(target=cannibalisation, args=(frame, agg_np, cb_table, cb_l, cb_level))  # Passing the list
+                    p.start()
+                    processes.append(p)
+                for p in processes:
+                    p.join()
+
+                output = pd.concat(frame)
+                results_df = pd.concat([results_df, output], ignore_index=True, sort =False)
+                results_df.reset_index(drop=True, inplace=True)
+                frame[:] = [] 
+
+                total_time_batch = round((time.time() - start_time_batch), 2)
+                logger.debug('Processing with batch size {a} took {b} secs...'.format(a=batchsize, b=total_time_batch))
+
+                logger.info('Results dataframe has {a} rows and {b} cols...'.format(a=results_df.shape[0], b=results_df.shape[1]))
+        
+        
+        # Convert all nulls to None
+        results_df = results_df.where((pd.notnull(results_df)), None)
+
+        total_time = round((time.time() - section_start_time) / 60, 1)
+        logger.info('Completed baseline processing in {a} mins...'.format(a=total_time))
+        
+        # upload the final dataframe onto Bigquery
+        logger.info('Uploading baseline table to Bigquery...')
+
+        if (i_sec == 0):
+            pandas_gbq.to_gbq(results_df, 'baseline_performance.cannibalisation', project_id=project_id, if_exists=bl_table_config)
+        else:
+            pandas_gbq.to_gbq(results_df, 'baseline_performance.cannibalisation', project_id=project_id, if_exists='append')
+
+        logger.info('Completed upload of section baseline to Bigquery...')
     
     total_time = round((time.time() - start_time) / 60, 1)
     logger.info('Completed baseline processing in {a} mins...'.format(a=total_time))
-   
-    pandas_gbq.to_gbq(results_df, '`baseline_performance.cannibalisation', project_id=project_id, if_exists='replace')
-
