25d1cf81e2c5e639a6f563b8fd418362581fbbff
simanli12 <56723489+simanli12@users.noreply.github.com>
2019-11-26 11:11:53 +0000

    test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pandas_gbq
+
+# Project ID
+project_id = "gum-eroski-dev"
+
+word = "date"
+date = "'2019-04-15'"
+
+sql = f""" CREATE OR REPLACE TABLE WIP.test2 AS
+       SELECT * FROM `gum-eroski-dev.WIP.baseline_dashboard` 
+       WHERE {word} = {date}; 
+       
+       CREATE OR REPLACE TABLE WIP.test3 AS
+       SELECT * FROM `gum-eroski-dev.WIP.baseline_dashboard` 
+       WHERE {word} = {date};"""
+       
+test = pandas_gbq.read_gbq(sql, project_id=project_id) 
