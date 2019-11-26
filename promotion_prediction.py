90a9cfb2a112b00e364cf57d6b6a80d0c25070c1
hamzajuzer10 <hamza.juzer10@gmail.com>
2019-11-26 18:47:37 +0000

    update

--- promotion_prediction.py
+++ promotion_prediction.py
@@ -1,1 +1,40 @@
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import numpy as np
+import pandas_gbq
+from tqdm import tqdm
+import time
+import logging
+
+
+
+
+
+
+
+
+
+def train_promotion_prediction_model(input_data, learning_rate, max_depth, subsample, colsample_bytree, n_estimators, objective,
+                                     gamma, alpha, lambda, test_size_perc, test_months_exclusion):
+
+    """train the promotion model during the promotion weeks
+    :Args
+        input_data(dataframe): dataframe containing the input data to train the model 
+        learning_rate(float): step size shrinkage used to prevent overfitting. Range is [0,1]
+        max_depth(int): determines how deeply each tree is allowed to grow during any boosting round
+        subsample(float): percentage of samples used per tree. Low value can lead to underfitting
+        colsample_bytree(float): percentage of features used per tree. High value can lead to overfitting
+        n_estimators(int): number of trees you want to build
+        objective(reg object): determines the loss function to be used like reg:linear for regression problems,
+        gamma(int): controls whether a given node will split based on the expected reduction in loss after the split. A higher value leads to fewer splits. Supported only for tree-based learners
+        alpha(int): L1 regularization on leaf weights. A large value leads to more regularization
+        lambda(int): L2 regularization on leaf weights and is smoother than L1 regularization
+        test_size_perc(float): train test splits
+        test_months_exclusion: identifies the months to be excluded from the training data set
+    :return:
+        xgboost model(model): xgboost ML model
+    """
+    
+    
 
