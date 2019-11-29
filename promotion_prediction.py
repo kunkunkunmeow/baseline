e89aaacec610391fb4af027b7fb8614f7cb9426c
hamzajuzer10 <hamza.juzer10@gmail.com>
2019-11-29 12:23:05 +0000

    udpate

--- promotion_prediction.py
+++ promotion_prediction.py
@@ -563,7 +563,7 @@ if __name__ == "__main__":
                                                                             learning_rate=0.05, # set between 0.01-0.05
                                                                             max_depth=100, # 100 for lightgbm, 50 for xgboost
                                                                             num_leaves = 250, # for lightgbm
-                                                                            n_iter=20000, # for lightgbm, no. of iterations
+                                                                            n_iter=4000, # for lightgbm, no. of iterations, 20000
                                                                             n_estimators = 150, # for xgboost, no of estimators
                                                                             train_size=0.8, # test train split
                                                                             test_months_exclusion=test_months_exclusion, # exclude certain months
