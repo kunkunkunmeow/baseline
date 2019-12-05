b092df3201c9691695a00035f2bf8716db60188e
antsoi <37077742+antsoi@users.noreply.github.com>
2019-12-05 19:06:19 +0000

    Update price_elasticity.py

--- price_elasticity.py
+++ price_elasticity.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
         #category_table['sku_root_id'] = category_table['sku_root_id'].astype(str)
         category_table['sku_root_id'] = pd.to_numeric(category_table['sku_root_id'])
         category_table['std_price_per_unit'] = pd.to_numeric(category_table['std_price_per_unit'])
-        category_table['avg_sale_qty'] = pd.to_numeric(category_table['avg_sale_qty'])
+        category_table['avg_sales_qty'] = pd.to_numeric(category_table['avg_sales_qty'])
         category_table['actual_price'] = pd.to_numeric(category_table['actual_price'])
         category_table['duration_weeks'] = pd.to_numeric(category_table['duration_weeks'])
         category_table['std_dev_sales_qty'] = pd.to_numeric(category_table['std_dev_sales_qty'])
