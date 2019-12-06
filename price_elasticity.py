406cd87b8d53498a8eb1c992961c9ed59b99546a
antsoi <37077742+antsoi@users.noreply.github.com>
2019-12-06 22:57:50 +0000

    Update price_elasticity.py

--- price_elasticity.py
+++ price_elasticity.py
@@ -284,10 +284,11 @@ def linear_reg(frame, agg_np, cost_per_unit_table, sku, max_limit, min_limit, mi
     
     standard_dev.append(df.std(axis=0)['gradient'])
     
-    cost_per_unit.append(cost_per_unit_table.loc[cost_per_unit_table['sku_root_id']==sku]['cost_per_unit'])
+    cost = cost_per_unit_table.loc[cost_per_unit_table['sku_root_id']==sku]['cost_per_unit']
+    cost_per_unit.append(cost)
     sku_id.append(sku)
     
-    opt_price = (intercept_sum + (-average_gradient*float(cost_per_unit)))/(-average_gradient*2)
+    opt_price = (intercept_sum + (-average_gradient*float(cost)))/(-average_gradient*2)
     optimal_price.append(opt_price)
     percentage = (opt_price-avg_price)/avg_price
     percentage_change.append(percentage)
