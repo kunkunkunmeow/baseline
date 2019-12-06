4337af1c9af3ce733292307dd036490998e7cb23
antsoi <37077742+antsoi@users.noreply.github.com>
2019-12-06 20:22:45 +0000

    Update price_elasticity_store_reg.py

--- price_elasticity_store_reg.py
+++ price_elasticity_store_reg.py
@@ -197,13 +197,13 @@ def linear_reg(frame, agg_np, cost_per_unit_table, sku, max_limit, min_limit, mi
     
     logger.info(f'{sku} - being processed...')
     
-    #logger.info(agg_np)
+    logger.info(agg_np)
         
     # set dataframe for each sku
     fullData = agg_np.loc[agg_np['sku_root_id']==sku]
     #df.loc[df['column_name'] == some_value]
     
-    #logger.info(agg_np)
+    logger.info(agg_np)
     
     # get store ids
     store_ids = fullData.store_id.unique()
@@ -222,8 +222,8 @@ def linear_reg(frame, agg_np, cost_per_unit_table, sku, max_limit, min_limit, mi
         data = fullData.loc[fullData['store_id']==store_id]
         Nfactor = data.mean(axis=0)['avg_sales_qty']
     
-        feat = data.actual_price
-        qty = data.avg_sales_qty
+        feat = data[['actual_price']]
+        qty = data[['avg_sales_qty']]
         
         X = feat
         y = qty
