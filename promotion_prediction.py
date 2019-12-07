af56c4f4ec7c22e34f4d62ee048c73a4d23c2296
hamzajuzer10 <hamza.juzer10@gmail.com>
2019-12-07 13:58:28 +0000

    Update promotion_prediction.py

--- promotion_prediction.py
+++ promotion_prediction.py
@@ -413,7 +413,8 @@ def train_promotion_prediction_model(input_data, input_features, cat_columns, mo
     # Fill remaining absent values in X with -999
     input_data.fillna(-999, inplace=True)
     
-    # Describe the dataset
+    # Describe the dataset, only numeric features are described
+    print("Summary of statistics for numeric features in input data:")
     print(input_data.describe())
     
     # Check data types
