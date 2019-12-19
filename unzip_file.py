07d73045c3c02e1a4a902ef62e0c3dec3b260f83
antsoi <37077742+antsoi@users.noreply.github.com>
2019-12-19 13:39:58 +0000

    Update unzip_file.py

--- unzip_file.py
+++ unzip_file.py
@@ -16,7 +16,7 @@ storage_client = storage.Client()
 
 bucket = storage_client.get_bucket("erk-data-feed")
 print(bucket.exists())
-blobs = storage_client.list_blobs("erk-data-feed", prefix="Working_folder/AT/ETL_test", delimiter='/')
+blobs = storage_client.list_blobs("erk-data-feed", prefix="*ETL_test", delimiter='/')
 
 for blob in blobs:
     print(blob.name)
