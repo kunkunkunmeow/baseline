15d6ad0f91280dd591a8e537d3b7cbe2d89e2ef1
antsoi <37077742+antsoi@users.noreply.github.com>
2019-12-19 11:16:33 +0000

    Update unzip_file.py

--- unzip_file.py
+++ unzip_file.py
@@ -18,9 +18,9 @@ bucket = "gs://erk-data-feed/Working_folder/AT/ETL_test"
 
 blobs = storage_client.list_blobs(bucket)
 
-blobs = [blob for blob in blobs.name]
+blobs = [blob.name for blob in blobs]
 
-path_to_file = "~/etl_test/" + blob[0]
+path_to_file = "~/etl_test/" + blobs[0]
 
 local_directory = os.fsencode("~/etl_test/")
 
@@ -64,6 +64,6 @@ def change_extension(old_extension, new_extension, directory):
 
 
 if __name__ == "__main__":
-    download_blob(bucket, blob[0], path_to_file)
+    download_blob(bucket, blobs[0], path_to_file)
     gunzip(path_to_file, path_to_file)
     change_extension(".dat", ".csv", local_directory)
