cc8784c4bdedc8393910584e3fc67c55194f2ab5
Anthony Tsoi <antsoi@deloitte.co.uk>
2019-12-20 10:53:03 +0000

    Added logger

--- unzip_file.py
+++ unzip_file.py
@@ -5,6 +5,10 @@ import os
 import gzip
 import shutil
 from pathlib import Path
+import pandas_gbq
+import pandas as pd
+import numpy as np
+import logging
 
 # Instantiates a client
 storage_client = storage.Client()
@@ -21,6 +25,29 @@ local_dir = os.path.abspath(home + "/etl_test/")
 # local_directory = os.fsencode("~/etl_test/")
 
 
+def initialise_logger():
+    # Set logger properties
+    logger = logging.getLogger("auto_etl_to_bq")
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler("auto_etl_to_bq.log")
+    fh.setLevel(logging.DEBUG)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     # bucket_name = "your-bucket-name"
@@ -29,11 +56,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     # check if file exists
     if os.path.exists(destination_file_name):
-        print(
-            "Blob {} already exists in {}.".format(
-                source_blob_name, destination_file_name
-            )
-        )
+        logger.info("Blob {} already exists in {}.".format(source_blob_name, destination_file_name))
     else:
         storage_client = storage.Client()
 
@@ -41,15 +64,11 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
         blob = bucket.blob(source_blob_name)
         blob.download_to_filename(destination_file_name)
 
-        print(
-            "Blob {} downloaded to {}.".format(source_blob_name, destination_file_name)
-        )
+        logger.info("Blob {} downloaded to {}.".format(source_blob_name, destination_file_name))
 
 
 def gunzip(source_filepath, dest_filepath, block_size=65536):
-    with gzip.open(source_filepath, "rb") as s_file, open(
-        dest_filepath, "wb"
-    ) as d_file:
+    with gzip.open(source_filepath, "rb") as s_file, open(dest_filepath, "wb") as d_file:
         while True:
             block = s_file.read(block_size)
             if not block:
@@ -71,7 +90,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     blob.upload_from_filename(source_file_name)
 
-    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+    logger.info("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
 
 def change_extension(old_extension, new_extension, directory):
@@ -85,24 +104,21 @@ def change_extension(old_extension, new_extension, directory):
 
 
 if __name__ == "__main__":
+    initialise_logger()
     for blob in blob_list:
         blob_fn = blob.split("/")[-1]
         download_blob(bucket, blob, os.path.abspath(local_dir + "/" + blob_fn))
-        if os.path.exists(
-            os.path.abspath(local_dir + "/" + blob_fn.split(".")[0] + ".csv")
-        ):
-            print(
-                "File {} already unzipped".format(
-                    os.path.abspath(local_dir + "/" + blob_fn)
-                )
+        if os.path.exists(os.path.abspath(local_dir + "/" + blob_fn.split(".")[0] + ".csv")):
+            logger.info(
+                "File {} already unzipped".format(os.path.abspath(local_dir + "/" + blob_fn))
             )
         else:
             gunzip(
                 os.path.abspath(local_dir + "/" + blob_fn),
                 os.path.abspath(local_dir + "/" + blob_fn.split(".")[0] + ".csv"),
             )
-        upload_blob(
-            bucket,
-            os.path.abspath(local_dir + "/" + blob_fn.split(".")[0] + ".csv"),
-            "Working_folder/AT/ETL_test_upload/" + blob_fn.split(".")[0] + ".csv",
-        )
+        # upload_blob(
+        #     bucket,
+        #     os.path.abspath(local_dir + "/" + blob_fn.split(".")[0] + ".csv"),
+        #     "Working_folder/AT/ETL_test_upload/" + blob_fn.split(".")[0] + ".csv",
+        # )
